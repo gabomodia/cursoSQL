9af67b6dc939e2c2e9e8434ac519e9b9958c3e35
gabomodia <gabrielmodiapozuelo@gmail.com>
2021-09-10 12:07:50 +0200

    creadoCapitulo5

---
+++ Capitulo5/rellenarBD.sql
@@ -0,0 +1,8 @@
+/*
+Este archivo contiene todos los datos necesarios para rellenar la base de datos. 
+Utilízalo al principio del capítulo 5, para trabajar los ejercicios con los mismos datos que yo.
+
+*/
+
+
+
